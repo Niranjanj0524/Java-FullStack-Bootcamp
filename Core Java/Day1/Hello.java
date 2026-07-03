69addaf0838cd21969cd918f5e47e8e6d51b8a5d
Niranjanj0524 <niranjanjagtap0524@gmail.com>
2026-07-03 13:57:49 +0530

    Day 1: Java Introduction

---
+++ Core Java/Day1/Hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("Welcome to Java FullStack");
+
+        System.out.println("Name :" + "Niranjan");
+        System.out.println("Collage :" + "RMDSTIC");
+        System.out.println("Branch :" + "Computer");
+
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Java\n Full\n Stack\n Developer");
+    }
+}
